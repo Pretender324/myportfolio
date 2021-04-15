6d834c2864e2db7bf2b4672592f5febaba6053be
Pretender324 <r1kuchicken5761@gmail.com>
2021-04-15 22:30:06 +0900

    about-me 作成

---
+++ components/CommonHeader.tsx
@@ -0,0 +1,15 @@
+import { Menu } from 'antd'
+import { Header } from 'antd/lib/layout/layout'
+import Link from 'next/link'
+
+export function CommonHeader() {
+    return (
+        <Header className="">
+            <Menu theme="dark" mode="horizontal">
+                <Menu.Item key="1"><Link href="/">Home</Link></Menu.Item>
+                <Menu.Item key="2"><Link href="/about-me">About Me</Link></Menu.Item>
+                <Menu.Item key="3"><Link href="#">Skill Set</Link></Menu.Item>
+            </Menu>
+        </Header>
+    )
+}
