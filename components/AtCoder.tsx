44f1b5bd181cede71ede13b320f9003f8cac0eb2
Pretender324 <r1kuchicken5761@gmail.com>
2021-04-20 23:44:08 +0900

    Homeに表示

--- components/AtCoder.tsx
+++ components/AtCoder.tsx
@@ -1,7 +1,46 @@
-import {Typography} from 'antd'
+import { notification, Typography } from "antd";
+import useSWR from 'swr'
+import { AtCoderHistory } from "../entities/atcoder";
 
-export function AtCoder() {
+const fetcher = url => fetch(url).then(res => res.json());
+
+
+function AtCoderColor({ rate }: { rate:number}) {
+  let color = ''
+  if (rate >= 2800) {
+    color = 'red'
+  } else if (rate >= 2400) {
+    color = 'orange'
+  } else if (rate >= 2000) {
+    color = 'yellow'
+  } else if (rate >= 1600) {
+    color = 'blue'
+  } else if (rate >= 1200) {
+    color = '#00C0C0'
+  } else if (rate >= 800) {
+    color = 'green'
+  } else if (rate >= 400) {
+    color = 'brown'
+  } else {
+    color = 'gray'
+  }
+  return <Typography.Text style={{color: color}}>{rate}</Typography.Text>
+}
+
+export default function AtCoder() {
+  const { data, error } = useSWR('/api/atcoder', fetcher)
+  if (error) return <Typography.Text>"An error has occurred."</Typography.Text>
+  if (!data) return <Typography.Text>"Loading..."</Typography.Text>;
+  if (data != null) {
+    console.log(data)
+    const newestContestData: AtCoderHistory = data.slice(-1)[0]
     return (
-        <Typography>Highest: 1249</Typography>
+      <>
+        <Typography.Paragraph>Highest: <AtCoderColor rate={1249} /></Typography.Paragraph>
+        <Typography.Paragraph>最新のコンテスト: {newestContestData.ContestName}
+          <br></br>
+        (Perf. <AtCoderColor rate={newestContestData.Performance}></AtCoderColor>)</Typography.Paragraph>
+      </>
     )
+  }
 }
--- pages/api/atcoder.ts
+++ pages/api/atcoder.ts
@@ -1,24 +1,24 @@
-import { NextApiRequest, NextApiResponse } from "next"
+import { NextApiRequest, NextApiResponse } from "next";
 
 async function getAtcoderHistory(req: NextApiRequest, res: NextApiResponse) {
-    const url = "https://atcoder.jp/users/Pretender/history/json"
-    try {
-        const result = await fetch(url)
-        res.status(200).json(await result.json())
-    } catch {
-        res.status(404)
-    }
-
+  const url = "https://atcoder.jp/users/Pretender/history/json";
+  try {
+    const result = await fetch(url);
+    const json = await result.json()
+    res.status(200).json(json);
+  } catch {
+    res.status(404);
+  }
 }
 
 export default async function atcoder(
-    req: NextApiRequest,
-    res: NextApiResponse
+  req: NextApiRequest,
+  res: NextApiResponse
 ) {
-    if (req.method === 'GET') {
-        await getAtcoderHistory(req, res)
-        return
-    }
+  if (req.method === "GET") {
+    await getAtcoderHistory(req, res);
+    return;
+  }
 
-    res.status(405).end()
+  res.status(405).end();
 }
