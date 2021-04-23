82e3c526c3110d84aff11fa42d44290b681f9a79
Pretender324 <r1kuchicken5761@gmail.com>
2021-04-23 14:34:13 +0900

    twitter埋め込み

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,72 +1,75 @@
-import { Col, Divider, Row, Typography } from 'antd'
-import Layout, { Content } from 'antd/lib/layout/layout'
-import React from 'react'
-import 'antd/dist/antd.css';
-import { Title } from '../components/Title';
-import AtCoder from '../components/AtCoder';
+import { Col, Divider, Row, Typography } from "antd"
+import Layout, { Content } from "antd/lib/layout/layout"
+import React from "react"
+import "antd/dist/antd.css"
+import { Title } from "../components/Title"
+import AtCoder from "../components/AtCoder"
 
 
 
 export default function Home() {
-  return (
-      <Layout>
-        <Content>
-          <Title text="Riku Adachi's Portfolio"></Title>
-          <Row justify="center" style={{paddingTop: "20px"}}>
-            <Col span={16}>
-              <Row justify="space-around">
-                <Col span={10}>
-                  <div style={{
-                    textAlign: "center",
-                    backgroundColor: "lightgray",
-                    padding: "20px",
-                    height: "70vh",
-                    width: "100%",
-                    borderRadius: "10px"
-                  }}>
-                    <Typography.Title level={3}>Recent Activity</Typography.Title>
-                    <Divider>AtCoder</Divider>
-                    <AtCoder />
-                    <Divider>Twitter</Divider>
-                  </div>
-                </Col>
-                <Col span={10}>
-                  <Row>
-                    <Col span={24} style={{ textAlign: "center" }}>
-                      <div style={{
-                        textAlign: "center",
-                        backgroundColor: "lightgray",
-                        padding: "20px",
-                        height: "40vh",
-                        width: "100%",
-                        borderRadius: "10px"
-                      }}>
-                        <Typography.Title level={3}>更新履歴</Typography.Title>
-                      </div>
-                      
-                    </Col>
-                  </Row>
-                  <Row>
-                    
-                    <Col span={24} style={{ textAlign: "center"}}>
-                      <div style={{
-                        textAlign: "center",
-                        backgroundColor: "lightgray",
-                        padding: "20px",
-                        marginTop: "5vh",
-                        height: "25vh",
-                        width: "100%",
-                        borderRadius: "10px"
-                      }}>
-                        <Typography.Title level={3}>Contact</Typography.Title>
-                      </div>
-                    </Col>
-                  </Row>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
-        </Content>
-      </Layout>
-  )
+	return (
+		<Layout>
+			<Content>
+				<Title text="Riku Adachi's Portfolio"></Title>
+				<Row justify="center" style={{ paddingTop: "20px" }}>
+					<Col span={20}>
+						<div style={{
+							textAlign: "center",
+							backgroundColor: "lightgray",
+							padding: "20px",
+							height: "70vh",
+							width: "100%",
+							borderRadius: "10px"
+						}}>
+							<Row justify="center">
+								<Typography.Title level={3}>Recent Activity</Typography.Title>
+							</Row>
+							<Row justify="space-around">
+								<Col span={10}>
+									<Divider>AtCoder</Divider>
+									<AtCoder />
+								</Col>
+								<Col span={10}>
+									<Divider>Twitter</Divider>
+									<a className="twitter-timeline" data-width="400" data-height="400" href="https://twitter.com/Pretend16474300?ref_src=twsrc%5Etfw">Tweets by Pretend16474300</a>
+									<script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+								</Col>
+							</Row>
+						</div>
+					</Col>
+				</Row>
+				<Row justify="center" style={{ marginTop: "20px", marginBottom: "40px" }}>
+					<Col span={20}>
+						<Row justify="space-between">
+							<Col span={11} style={{ textAlign: "center" }}>
+								<div style={{
+									textAlign: "center",
+									backgroundColor: "lightgray",
+									padding: "20px",
+									height: "20vh",
+									width: "100%",
+									borderRadius: "10px"
+								}}>
+									<Typography.Title level={3}>更新履歴</Typography.Title>
+								</div>
+							</Col>
+							<Col span={11} style={{ textAlign: "center"}}>
+								<div style={{
+									textAlign: "center",
+									backgroundColor: "lightgray",
+									padding: "20px",
+									height: "20vh",
+									width: "100%",
+									borderRadius: "10px"
+								}}>
+									<Typography.Title level={3}>Contact</Typography.Title>
+								</div>
+							</Col>
+						</Row>
+					</Col>
+				</Row>
+			</Content>
+		</Layout>
+	)
 }
