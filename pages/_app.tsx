bfaf4b6f55eb776de6b58e56e80623bf49027eb4
Pretender324 <r1kuchicken5761@gmail.com>
2021-04-23 12:13:42 +0900

    完了

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import { Layout } from 'antd'
 import { CommonHeader } from '../components/CommonHeader'
 import '../styles/globals.css'
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout
-      style={{ minHeight: "100vh" }}>
-      <CommonHeader />
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>Riku Adachi</title>
+      </Head>
+      <Layout
+        style={{ minHeight: "100vh" }}>
+        <CommonHeader />
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   )
 
 }
