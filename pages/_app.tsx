129700ab8726dbb72858f46999957b72078329b5
Pretender324 <r1kuchicken5761@gmail.com>
2021-04-25 21:50:05 +0900

    antdのCSSを修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,24 @@
-import { Layout } from 'antd'
-import { CommonHeader } from '../components/CommonHeader'
-import '../styles/globals.css'
-import Head from 'next/head';
+import { Layout } from "antd"
+import { CommonHeader } from "../components/CommonHeader"
+import "../styles/globals.css"
+import Head from "next/head"
+import React from "react"
+import type { AppProps } from "next/app"
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <div>
-      <Head>
-        <title>Riku Adachi</title>
-      </Head>
-      <Layout
-        style={{ minHeight: "100vh" }}>
-        <CommonHeader />
-        <Component {...pageProps} />
-      </Layout>
-    </div>
-  )
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<div>
+			<Head>
+				<title>Riku Adachi</title>
+			</Head>
+			<Layout
+				style={{ minHeight: "100vh" }}>
+				<CommonHeader />
+				<Component {...pageProps} />
+			</Layout>
+		</div>
+	)
 
 }
 
