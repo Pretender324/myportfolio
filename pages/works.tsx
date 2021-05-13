3dcb5eecbd8d6be4d7f58799e5e02f6011dc2508
Pretender324 <r1kuchicken5761@gmail.com>
2021-05-13 14:47:22 +0900

    アニメーション追加

--- pages/works.tsx
+++ pages/works.tsx
@@ -1,5 +1,6 @@
 import { Card, Col, Empty, Layout, Row, Tag, Typography } from "antd";
 import { Content } from "antd/lib/layout/layout";
+import { motion } from "framer-motion";
 import { ReactChild } from "react";
 import { Title } from "../components/Title";
 import { Language } from "../entities/works";
@@ -26,104 +27,111 @@ export default function Works() {
   const { Meta } = Card;
 
   return (
-    <Layout>
-      <Content>
-        <Title text="Works"></Title>
-        <Typography.Paragraph style={{ textAlign: "center" }}>
-          私がこれまで作ってきた作品を紹介します。
-        </Typography.Paragraph>
-        <Row justify="space-around">
-          <WorkContent>
-            <Card
-              hoverable
-              cover={
-                <img
-                  alt="example"
-                  src="howtoteach.png"
-                  style={{ height: "200px" }}
+    <motion.div
+      initial={{ x: "-100vw" }}
+      animate={{ x: 0 }}
+      transition={{ ease: "backOut", duration: 1 }}
+      exit={{ x: "-100vw" }}
+    >
+      <Layout>
+        <Content>
+          <Title text="Works"></Title>
+          <Typography.Paragraph style={{ textAlign: "center" }}>
+            私がこれまで作ってきた作品を紹介します。
+          </Typography.Paragraph>
+          <Row justify="space-around">
+            <WorkContent>
+              <Card
+                hoverable
+                cover={
+                  <img
+                    alt="example"
+                    src="howtoteach.png"
+                    style={{ height: "200px" }}
+                  />
+                }
+                style={{ height: "360px" }}
+              >
+                <Meta
+                  title="How to Teach"
+                  description="初めて作ったWebアプリです。塾講師のバイトの悩みを共有する、という内容です。"
                 />
-              }
-              style={{ height: "360px" }}
-            >
-              <Meta
-                title="How to Teach"
-                description="初めて作ったWebアプリです。塾講師のバイトの悩みを共有する、という内容です。"
-              />
-              <LanguageTags
-                languages={[
-                  {
-                    name: "HTML/CSS",
-                    color: "orange",
-                  },
-                  {
-                    name: "JavaScript",
-                    color: "purple",
-                  },
-                  {
-                    name: "Ruby on Rails",
-                    color: "red",
-                  },
-                ]}
-              ></LanguageTags>
-            </Card>
-          </WorkContent>
-          <WorkContent>
-            <Card
-              hoverable
-              cover={
-                <Empty
-                  style={{
-                    margin: "0",
-                  }}
-                  imageStyle={{ height: "170px", lineHeight: "170px" }}
+                <LanguageTags
+                  languages={[
+                    {
+                      name: "HTML/CSS",
+                      color: "orange",
+                    },
+                    {
+                      name: "JavaScript",
+                      color: "purple",
+                    },
+                    {
+                      name: "Ruby on Rails",
+                      color: "red",
+                    },
+                  ]}
+                ></LanguageTags>
+              </Card>
+            </WorkContent>
+            <WorkContent>
+              <Card
+                hoverable
+                cover={
+                  <Empty
+                    style={{
+                      margin: "0",
+                    }}
+                    imageStyle={{ height: "170px", lineHeight: "170px" }}
+                  />
+                }
+                style={{ height: "360px" }}
+              >
+                <Meta
+                  title="Riku Adachi's Portfolio"
+                  description="このポートフォリオです。自己紹介のために作りました。"
                 />
-              }
-              style={{ height: "360px" }}
-            >
-              <Meta
-                title="Riku Adachi's Portfolio"
-                description="このポートフォリオです。自己紹介のために作りました。"
-              />
-              <LanguageTags
-                languages={[
-                  {
-                    name: "HTML/CSS",
-                    color: "orange",
-                  },
-                  {
-                    name: "TypeScript",
-                    color: "blue",
-                  },
-                  {
-                    name: "Next.js",
-                    color: "purple",
-                  },
-                ]}
-              ></LanguageTags>
-            </Card>
-          </WorkContent>
-          <WorkContent>
-            <Card
-              hoverable
-              cover={
-                <Empty
-                  style={{
-                    margin: "0",
-                  }}
-                  imageStyle={{ height: "170px", lineHeight: "170px" }}
+                <LanguageTags
+                  languages={[
+                    {
+                      name: "HTML/CSS",
+                      color: "orange",
+                    },
+                    {
+                      name: "TypeScript",
+                      color: "blue",
+                    },
+                    {
+                      name: "Next.js",
+                      color: "purple",
+                    },
+                  ]}
+                ></LanguageTags>
+              </Card>
+            </WorkContent>
+            <WorkContent>
+              <Card
+                hoverable
+                cover={
+                  <Empty
+                    style={{
+                      margin: "0",
+                    }}
+                    imageStyle={{ height: "170px", lineHeight: "170px" }}
+                  />
+                }
+                style={{ height: "360px" }}
+              >
+                <Meta
+                  title="Swistory(製作中)"
+                  description="水泳の練習メニューを記録するアプリです。"
                 />
-              }
-              style={{ height: "360px" }}
-            >
-              <Meta
-                title="Swistory(製作中)"
-                description="水泳の練習メニューを記録するアプリです。"
-              />
-              <LanguageTags languages={[]}></LanguageTags>
-            </Card>
-          </WorkContent>
-        </Row>
-      </Content>
-    </Layout>
+                <LanguageTags languages={[]}></LanguageTags>
+              </Card>
+            </WorkContent>
+          </Row>
+        </Content>
+      </Layout>
+    </motion.div>
   );
 }
